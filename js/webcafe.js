dc5f0443343e0049bc0227d2ff86452a7957672d
plaintaste <sooy8eo@gmail.com>
2017-08-14 18:03:13 +0900

    인터렉션 및 마크다운 학습

---
+++ js/webcafe.js
@@ -0,0 +1,9 @@
+$(function() {
+    $('.btn-menubar').on('click', function() {
+        $(this).parent().siblings().toggleClass('main-menu-act');
+    });
+    $('.menubar').on('click', function() {
+        $(this).parent().siblings().toggleClass('main-menu-act');
+    });
+
+});
